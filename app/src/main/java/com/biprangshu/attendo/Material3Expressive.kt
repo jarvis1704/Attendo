662c2ae90fa974fbbb5c4ac8f2f55a221ee4739b
Biprangshu Das <dasbiprangshu@gmail.com>
2025-07-20 13:53:51 +0530

    m3 expressive test

--- app/src/main/java/com/biprangshu/attendo/Material3Expressive.kt
+++ app/src/main/java/com/biprangshu/attendo/Material3Expressive.kt
@@ -1,10 +1,14 @@
 package com.biprangshu.attendo
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderState
 import androidx.compose.material3.Switch
@@ -15,8 +19,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun Material3ExpressiveTest(modifier: Modifier = Modifier) {
     var switchState by remember{ mutableStateOf(true) }
@@ -35,4 +41,6 @@ fun Material3ExpressiveTest(modifier: Modifier = Modifier) {
             }
         },
     )
+    Spacer(Modifier.height(8.dp))
+    LoadingIndicator()
 }
