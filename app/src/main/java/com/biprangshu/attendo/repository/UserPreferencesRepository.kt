e7e9b51e162ba3cfb1fa6edae743897b13e6fbb0
Biprangshu Das <dasbiprangshu@gmail.com>
2025-07-22 13:17:27 +0530

    added datastore repository

---
+++ app/src/main/java/com/biprangshu/attendo/repository/UserPreferencesRepository.kt
@@ -0,0 +1,71 @@
+package com.biprangshu.attendo.repository
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+private val Context.dataStore: DataStore<androidx.datastore.preferences.core.Preferences> by preferencesDataStore(name = "user_preferences")
+
+@Singleton
+class UserPreferencesRepository @Inject constructor(
+    @ApplicationContext private val context: Context
+){
+
+    private val dataStore = context.dataStore
+
+
+
+    companion object{
+
+        private val IS_FIRST_APP_OPEN = booleanPreferencesKey("first_app_open")
+        private val REQUIRED_PERCENTAGE = intPreferencesKey("required_percentage")
+
+    }
+
+    val isFirstAppOpen: Flow<Boolean> = dataStore.data.map { preferences ->
+        preferences[IS_FIRST_APP_OPEN] ?: true
+    }
+
+    val requiredPercentage: Flow<Int> = dataStore.data.map {
+        preferences ->
+        preferences[REQUIRED_PERCENTAGE] ?: 75
+    }
+
+    suspend fun firstAppOpen(isFirstAppOpen: Boolean){
+        dataStore.edit { preferences ->
+            preferences[IS_FIRST_APP_OPEN] = isFirstAppOpen
+        }
+    }
+
+    suspend fun changeRequiredPercentage(requiredPercentage: Int){
+        dataStore.edit {
+            preferences ->
+            preferences[REQUIRED_PERCENTAGE] = requiredPercentage
+        }
+    }
+
+
+    suspend fun clearUserData() {
+        dataStore.edit { preferences ->
+            preferences.remove(IS_FIRST_APP_OPEN)
+            preferences.remove(REQUIRED_PERCENTAGE)
+        }
+    }
+
+    suspend fun nukeAllPreferences(){
+        //as it says, use with caution
+        dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+}
+
