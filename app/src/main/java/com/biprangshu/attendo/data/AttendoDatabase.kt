cefdcdf98c7c0af056717b19f647e9052ca44645
Biprangshu Das <dasbiprangshu@gmail.com>
2025-08-04 21:55:36 +0530

    created database version 2

--- app/src/main/java/com/biprangshu/attendo/data/AttendoDatabase.kt
+++ app/src/main/java/com/biprangshu/attendo/data/AttendoDatabase.kt
@@ -4,8 +4,9 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 
-@Database(entities = [Subject::class], version = 1, exportSchema = false)
+@Database(entities = [Subject::class, AttendanceRecord::class], version = 2, exportSchema = false)
 abstract class AttendoDatabase: RoomDatabase() {
 
     abstract fun subjectDao(): SubjectDao
+    abstract fun attendenceDao(): AttendenceDao
 }
